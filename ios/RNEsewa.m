b083b92bb4976d65186aa2e640c420b15757ade8
Thegana <thegana@MacBook-Pro-2.local>
2020-01-04 12:21:04 +0545

    ios tests

--- ios/RNEsewa.m
+++ ios/RNEsewa.m
@@ -3,12 +3,12 @@
 
 @implementation RNEsewa
 
-// - (dispatch_queue_t)methodQueue
-// {
-//     return dispatch_get_main_queue();
-// }
-
 RCT_EXPORT_MODULE();
+RCT_EXPORT_METHOD(getModuleList: (RCTResponseSenderBlock)callback)
+{
+   NSArray *nativeModuleList = @[@"react-native-fbsdk", @"react-native-camera", @"react-native-maps"];
+   callback(@[nativeModuleList]);
+}
 // @interface RCT_EXTERN_REMAP_MODULE(RNEsewa, Esewa, NSObject)
 //     // RCT_EXPORT_MODULE()
 //     // RCT_EXTERN_METHOD(increment)
